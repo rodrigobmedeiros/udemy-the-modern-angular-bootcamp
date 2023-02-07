4f9e8a60d6a3246ecc8b5e61f49d613bfc655d41
Rodrigo Bernardo Medeiros <rbmedeiros@esss.co>
2023-02-06 21:31:08 -0300

    Implement text comparison changing colors depend on the char is equal or not

--- typing/src/app/app.component.ts
+++ typing/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'typing';
+  randomSentence: string = faker.lorem.sentence();
+  isCorrect: boolean = false;
+  enteredText: string = '';
+
+  constructor() {}
+
+  detectChangeInText(event: Event) {
+    this.enteredText = (event.target as HTMLInputElement).value;
+    this.isCorrect = this.enteredText === this.randomSentence;
+  }
 }
