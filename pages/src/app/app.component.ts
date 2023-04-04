7d2dfdaff15a8fac51dd760193359d2f5ee44104
rodrigo bernardo medeiros <rodrimedeiros@gmail.com>
2023-04-03 21:17:42 -0300

    Implement pages project

--- pages/src/app/app.component.ts
+++ pages/src/app/app.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  images = [
+    {
+      title: 'At the beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80'
+    },
+    {
+      title: 'At the mountain',
+      url: 'https://images.unsplash.com/photo-1465056836041-7f43ac27dcb5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80'
+    },
+    {
+      title: 'At the river',
+      url: 'https://images.unsplash.com/photo-1506355683710-bd071c0a5828?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+      title: 'At the forest',
+      url: 'https://images.unsplash.com/photo-1425913397330-cf8af2ff40a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
+    },
+    {
+      title: 'At the city',
+      url: 'https://images.unsplash.com/photo-1519501025264-65ba15a82390?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+    },
+  ]
+
+  currentImageIndex = 0;
+
+  public setCurrentImage(i: number): void {
+    this.currentImageIndex = i;
+  }
+
+  public incrementImageIndex(): void {
+    // if (this.currentImageIndex < this.images.length - 1) {
+      this.currentImageIndex += 1;
+    // }
+  }
+
+  public decrementImageIndex(): void {
+    // if (this.currentImageIndex > 0) {
+      this.currentImageIndex -= 1;
+    // }
+  }
 }
